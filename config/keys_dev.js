bbee9a738b3beb9215767a37b2299899a0645ddf
Brandon Wie <it.brandonwie@gmail.com>
2019-09-16 19:31:55 +0400

    secret changed

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI: "mongodb+srv://brandonwie:Marianka3@cluster0-biupg.gcp.mongodb.net/test?retryWrites=true&w=majority",
-    secret: "veryverysecret"
+    secret: "brandonwie"
 }
